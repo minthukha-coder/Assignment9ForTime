d5e1aaa1acd985de1d96294118166e05f07f237f
Min Thu Kha <112501430+minthukha-coder@users.noreply.github.com>
2023-03-16 16:48:07 +0630

    Create main_zoom_online_bank.cpp

---
+++ main_zoom_online_bank.cpp
@@ -0,0 +1,18 @@
+//
+// Created by National Cyber City on 2/6/2023.
+//
+
+
+#include "stdio.h"
+#include "zoom_online_bank.h"
+
+int main(){
+    space_counter();
+    loadingAllDataFromFile();
+    printingAllData();
+    welcome();
+
+//    integer_to_char(1000);
+
+    return 0;
+}
